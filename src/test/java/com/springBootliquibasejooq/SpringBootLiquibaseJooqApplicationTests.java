bfd538483a07c33c02fe6165a02d93a7b3838ede
Mayank Devrani <53875589+kakarot987@users.noreply.github.com>
2021-07-02 22:05:22 +0530

    init

---
+++ src/test/java/com/springBootliquibasejooq/SpringBootLiquibaseJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootliquibasejooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiquibaseJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
